dbb7b4ae879550c1221304b7b35c86f5697656b0
ItzNesbroDev <asnebseer3@gmail.com>
2022-02-20 20:47:57 +0530

    feat(*): updated to js

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 import getYoutubeId from "get-youtube-id";
 
-export default function Viewer(url: string) {
-  let id = getYoutubeId(url);
+function thumbnailViewer(url) {
+let id = getYoutubeId(url);
 
   if (!id && url.length === 11) {
     id = url;
   }
   return "http://img.youtube.com/vi/" + id + "/hqdefault.jpg";
 }
+
+module.exports.thumbnailViewer = thumbnailViewer;
